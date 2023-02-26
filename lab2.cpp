ee7d2dc2487d6cce412a582edf32d4aa6f025e3b
Ivan-PIA <wind.vanya@gmail.com>
2023-02-27 00:43:46 +0700

    second version move_point

--- lab2.cpp
+++ lab2.cpp
@@ -4,64 +4,102 @@
 #include <thread>
 #include <string>
 #include <conio.h>
+#include <time.h>
 using namespace std;
 
-struct c
-{
+#define N 1000
 
+typedef struct Object
+{
     int x;
     int y;
-};
+    int key;
+    int **memory;
+
+}object;
+
+
+
+
+void randomazer(int sum_move, int sum_obj, int scale, object *mass_obj){
+    int move;
+    int cur = rand()%5;
+    for(move=0; move < sum_move; move++){
+        if (cur == 0){
+            mass_obj->x=mass_obj->x;
+            mass_obj->y=mass_obj->y;
+        }
+        else if (cur == 1 & mass_obj->y+scale>0)
+            mass_obj->y+=scale;
+        
+        else if (cur == 2 & mass_obj->y-scale<1000)
+            mass_obj->y-=scale;
+
+        else if (cur == 3 & mass_obj->x+scale<1000)
+            mass_obj->x+=scale;
+            
+        else if (cur == 4 & mass_obj->x-scale>0) 
+            mass_obj->x-=scale;  
+
+        mass_obj->memory[move][0]=mass_obj->x;
+        mass_obj->memory[move][1]=mass_obj->y;
+    
+    } 
+}
+
+
 
 
 int main(){
 
-int i,j,k,c,N;
+    srand(time(NULL));
+
+int i,j,mov,scale, sum_obj, sum_move, obj;
+
+object *mass_obj;
 
 
-printf("Введите размер поля: ");
+cout << "Enter scale: ";
+cin >> scale;
 
-scanf("%d",&N);
-char map[N][N];
-//printf("Введите начальное положение ");
+cout << "\nEnter amount object: ";
+cin >> sum_obj;
 
-
-
-int look= 0, o=0, p=1 ;
-
+cout << "\nEnter amount move: ";
+cin >> sum_move;
 
 
-    
-for ( c=0; c<3;c++)    {
-    p++;
-    o++;
+mass_obj = (object*)malloc(sum_obj*sizeof(object)); 
+
+randomazer(sum_move, sum_obj, scale, mass_obj);
+
+
+int look = 0; 
+//PAINT
+cout << "sfjhkf" ;
+for (mov = 0; mov < sum_move; mov++){
 
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    system("cls");
-    for (i = 0; i < N; i++){
-        for(j = 0; j < N; j++){
-            look=0;
-            for(k=0; k<1; k++){
-                if(i==o & j == p){
-                    look = 1 ; 
-                     printf("0");
-                }
-                else look = 0;
-                
-                if (look == 0){
+     system("cls");
+    
+    for(i = 0; i < N/scale; i++){
+        for(j = 0; N < N/scale; j++){
+            for(obj=0; obj < N/1000; obj++){
+                if(i == mass_obj->x/scale && j == mass_obj->y/scale){
                     
-                    printf("*");
-
+                    cout << "@ ";
                 }
-           
+                if(i != mass_obj->x/scale || j != mass_obj->y/scale){
+                    cout << "* ";
+                }
             }
-
         }
-        printf("\n");
+        cout << "\n";
     }
-    printf("\n");
-
+    cout << "\n";
 }
+
+
 
 
 }
