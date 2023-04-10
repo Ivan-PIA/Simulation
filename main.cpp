f33ac93f5dd0111f62d5d9a34c9ebcfe78bb8bc2
Ivan-PIA <wind.vanya@gmail.com>
2023-04-10 22:05:00 +0700

    add description

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ int main(){
     choice_method(1,stek,count_obj,scale,count_mov);
 
     for(mov=0; mov < count_mov; mov++){
-		paint(scale,mov, count_obj, stek);
+		  paint(scale,mov, count_obj, stek);
     }
 
     interface_distance(stek,count_obj,scale,count_mov);
